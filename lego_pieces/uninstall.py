3b2108db63c9bfe047b53d0b72a65287317f9a36
Mustafa Özkan İr <mustafa54ir@gmail.com>
2025-06-15 09:47:26 +0300

    Add notepad++ installers to the repository!

--- lego_pieces/uninstall.py
+++ lego_pieces/uninstall.py
@@ -3,11 +3,19 @@ import os
 
 def uninstall_notepadpp():
     uninstall_path = r"C:\Program Files (x86)\Notepad++\uninstall.exe"
-
+    alternative_uninstall_path = r"C:\Program Files\Notepad++\uninstall.exe"
     if os.path.exists(uninstall_path):
         print("🗑️ Uninstalling Notepad++ silently...")
         try:
             subprocess.run([uninstall_path, "/S"], check=True)
+            print("✅ Notepad++ uninstalled.")
+        except subprocess.CalledProcessError as e:
+            print("❌ Uninstallation failed:", e)
+
+    elif os.path.exists(alternative_uninstall_path):
+        print("🗑️ Uninstalling Notepad++ silently...")
+        try:
+            subprocess.run([alternative_uninstall_path, "/S"], check=True)
             print("✅ Notepad++ uninstalled.")
         except subprocess.CalledProcessError as e:
             print("❌ Uninstallation failed:", e)
