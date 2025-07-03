2a0b78a086354a5999240e1e0863305dcca6536e
Mustafa Özkan İr <mustafa54ir@gmail.com>
2025-07-03 09:17:57 +0300

    Add more bugs to the dataset!

--- install_test.py
+++ install_test.py
@@ -1,5 +1,6 @@
 from fixpad.env_manager import switch_to_version, launch_notepadpp
 
-switch_to_version("npp.7.6.4")
+switch_to_version("npp.8.4.8")
 launch_notepadpp()
-
+steps_to_reproduce = ""
+print(steps_to_reproduce)
